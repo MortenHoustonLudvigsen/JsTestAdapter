dc0aaeb3b027da9a13170704c156c7b467b8664e
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-03-18 18:05:10 +0100

    Progress

---
+++ JsTestAdapter/Tools/Install.ps1
@@ -0,0 +1,50 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function CreateTextFile($path, $text)
+{
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    [System.IO.File]::WriteAllLines($path, $text, $Utf8NoBomEncoding)
+}
+
+function GetRelativePath($base, $path)
+{
+    $tmp = Get-Location
+    Set-Location $base
+    $result = Resolve-Path -relative $path
+    Set-Location $tmp
+    return $result.Replace("\", "/")
+}
+
+
+$projectDir = [System.IO.Path]::GetDirectoryName($project.FullName)
+
+$packageDir = GetRelativePath $projectDir $installPath
+$TestServerPath = Join-Path $packageDir "TestServer"
+$TestServerPath = $TestServerPath.Replace("\", "/")
+
+Write-Host "projectDir: $projectDir"
+Write-Host "packageDir: $packageDir"
+Write-Host "TestServerPath: $TestServerPath"
+
+
+$JsTestAdapterConfigPath = Join-Path $projectDir "JsTestAdapter.json"
+ 
+CreateTextFile $JsTestAdapterConfigPath @"
+{
+    "Package": "$packageDir",
+    "TestServer": "$TestServerPath"
+}
+"@
+
+$project.ProjectItems.AddFromFile($JsTestAdapterConfigPath)
+
+
+# 
+# 
+# Write-Host "installPath: $installPath"
+# Write-Host "toolsPath: $toolsPath"
+# Write-Host "package: $package"
+# Write-Host "TestServerPath: $TestServerPath"
+# Write-Host "projectDir: $projectDir"
+# Write-Host "testServerPackage: $testServerPackage"
+# 
--- TestTestAdapter/TestServer/Logger.ts
+++
@@ -1,8 +0,0 @@
-﻿interface Logger {
-    info(message: string, ...args: any[]): void;
-    warn(message: string, ...args: any[]): void;
-    error(message: string, ...args: any[]): void;
-    debug(message: string, ...args: any[]): void;
-}
-
-export = Logger;
