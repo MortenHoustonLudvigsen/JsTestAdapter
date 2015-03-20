60fa8433fe634c73fea2483519b87b8794567bdc
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-03-20 22:13:56 +0100

    Add publish grunt task

--- JsTestAdapter/gruntfile.js
+++ JsTestAdapter/gruntfile.js
@@ -3,6 +3,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        clean: {
+            dist: ['dist']
+        },
+
         nugetpack: {
             dist: {
                 src: 'JsTestAdapter.nuspec',
@@ -11,12 +15,19 @@ module.exports = function (grunt) {
                     Version: '<%= pkg.version %>'
                 }
             }
+        },
+
+        nugetpush: {
+            dist: {
+                src: 'dist/*.nupkg'
+            }
         }
     });
 
-    grunt.registerTask('AfterBuild', ['nugetpack:dist']);
-    grunt.registerTask('default', ['']);
+    grunt.registerTask('AfterBuild', ['clean:dist', 'nugetpack:dist']);
+    grunt.registerTask('Publish', ['clean:dist', 'nugetpack:dist', 'nugetpush:dist']);
+    grunt.registerTask('default', ['AfterBuild']);
 
     grunt.loadNpmTasks('grunt-nuget');
-    grunt.loadNpmTasks('grunt-nuget');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 };
